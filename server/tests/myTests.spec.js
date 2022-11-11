fa047d527a85f955006955975f65581b00efb501
“liambrockpy” <liambrock72@gmail.com>
2022-11-11 12:08:31 +0000

    change to english spelling

--- server/tests/myTests.spec.js
+++ server/tests/myTests.spec.js
@@ -17,39 +17,39 @@ afterAll(done => {
     api.close(done)
 })
 
-let testColor = {
+let testColour = {
     "id": 3,
     "name": "blue",
     "hex": "#0343df"
 }
 
-describe('Read colors', () => {
+describe('Read colours', () => {
     it('should respond to GET "/" with status 200', done => {
         request(api).get('/').expect(200, done)
     })
 
-    it('should read a color by ID', (done) => {
+    it('should read a colour by ID', (done) => {
         request(api)
-            .get('/colors/3')
+            .get('/colours/3')
             .expect(200)
             .expect(testColor, done)
     })
 
     it('should respond to non IDs with 404', done => {
-        request(api).get('/colors/no').expect(404, done)
+        request(api).get('/colours/no').expect(404, done)
     })
 })
 
-describe('Create colors', () => {
+describe('Create colours', () => {
     it('should send a 405 when writing to root url', (done) => {
         request(api).post('/')
             .expect(405, done)
     })
 
-    it('should respond to a POST "/colors" with 201', done => {
+    it('should respond to a POST "/colours" with 201', done => {
         request(api)
-            .post('/colors')
-            .send(testColor)
+            .post('/colours')
+            .send(testColour)
             .expect(201)
             .expect({
                 id: 3,
@@ -59,17 +59,17 @@ describe('Create colors', () => {
     })
 })
 
-describe('Update colors', () => {
-    test.todo('should respond to a PUT "/colors/:id" with status 200')
+describe('Update colours', () => {
+    test.todo('should respond to a PUT "/colours/:id" with status 200')
 })
 
-describe('Delete colors', () => {
-    it('should respond to DELETE "/colors/:id" with status 204', async () => {
+describe('Delete colours', () => {
+    it('should respond to DELETE "/colours/:id" with status 204', async () => {
         await request(api)
-            .delete('/colors/2')
+            .delete('/colours/2')
             .expect(204)
 
-        const updatedColors = await request(api).get('/colors')
+        const updatedColors = await request(api).get('/colours')
         expect(updatedColors.body.length).toBe(dataLength - 1)
     })
 })
