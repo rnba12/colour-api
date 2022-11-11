be5e09215b0e0769464ed61b938622c38501db80
rnba12 <rnba12@yahoo.co.uk>
2022-11-11 14:33:38 +0000

    updated controller and model files

--- server/app.js
+++ server/app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const colourRoutes = require('./controllers/colour')
 const bodyParser = require('body-parser')
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.status(200).send("Colours API: Welcome")
+})
+
+app.post('/', (req, res) => {
+    res.status(405).send("Method not allowed");
 })
 
 app.listen(port, () => {
